d5fb24b94b01c2ec9a77d61073385c9ad89a197d
Cihan Çakır <cihacakiroglu@gmail.com>
2026-05-27 17:37:47 +0300

    ReferenceData module registration

--- Modules/Identity/src/Aizen.Modules.Identity/Program.cs
+++ Modules/Identity/src/Aizen.Modules.Identity/Program.cs
@@ -8,7 +8,6 @@ using Aizen.Modules.Identity.Repository.Context;
 using Aizen.Core.Domain.Abstraction.Extension;
 using Aizen.Modules.InktaviaStore.Repository;
 using Aizen.Modules.Identity.Extensions;
-using Aizen.Modules.Identity.Services;
 
 var builder = AizenApplicationBuilder.CreateBuilder(new AizenAppInfo
 {
@@ -34,7 +33,6 @@ builder.Services.Configure<ApplicationSettings>(builder.Configuration.GetSection
 builder.Services.AddInktaviaService(builder.Configuration).AddInktaviaRepository();
 
 builder.Services.AddInktaviaAuthorizationPolicies();
-builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
 
 builder.Services.AddAizenErrorLocalization(builder.Configuration, typeof(IdentityDbContext).Assembly);
 
---
+++ Modules/ReferenceData/src/Aizen.Modules.ReferenceData.Abstraction/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Aizen.Modules.ReferenceData.Abstraction;
+
+public class Class1
+{
+
+}
